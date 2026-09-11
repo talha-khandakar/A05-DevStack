1ae8fb6396ede0f2feda8dcbccf1ea0adf7b1101
talha-khandakar <talhakhandakarshreyas@gmail.com>
2026-09-11 14:00:15 +0600

    json data cretation done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { Suspense, useState } from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Technologies from "./components/Technologies";
+import type { technologiesType } from "./Type";
+
+const technologyFetching = async (): Promise<technologiesType[]> => {
+  const res = await fetch("../public/Data.json");
+  const data = await res.json();
+  return data;
+};
+
+const [technologyPromises] = useState(() => technologyFetching());
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Suspense fallback={<h2>Techologies Loading...</h2>}>
+        <Technologies />
+      </Suspense>
     </>
   );
 }
